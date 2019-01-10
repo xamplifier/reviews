22e9f4c260305f0aadc0c14b4dd9d091917881a0
Apostolos Flessas <abcy122@googlemail.com>
2019-01-10 17:53:33 +0200

    adding paginated rating results

--- src/Facebook.php
+++ src/Facebook.php
@@ -5,11 +5,12 @@ namespace Xamplifier\Reviews;
 
 use \DateTimeInterface;
 use Xamplifier\Reviews\EndPoint;
+use Illuminate\Support\Arr;
 use Facebook\Facebook as FacebookSdk;
 
 class Facebook extends Base
 {
-    public function __construct(array $config)
+    public function __construct($config)
     {
         parent::__construct();
 
@@ -19,16 +20,27 @@ class Facebook extends Base
         ]));
     }
 
+
+    /**
+     *  Gets facebook rating data
+     *  GraphEdge object
+     *
+     * @param  Array $e the endpoints
+     * @return Object $data the GraphEdge object
+     */
     public function getReviews(EndPoint $e)
     {
+        //get the graph edge rating data
+        $fields = 'open_graph_story,created_time,has_rating,has_review,rating,recommendation_type,review_text,reviewer';
+
         try {
-            $url = sprintf('/%s/?fields=ratings', $e->siteid);
+            $url = sprintf('/%s/ratings?fields=%s', $e->siteid, $fields);
             $response = $this->library->get(
                 $url,
                 $e->token
             );
-            $data = $response->getGraphNode();
 
+            $data = $response->getGraphEdge();
         } catch (\Facebook\Exceptions\FacebookResponseException $e) {
             echo 'Graph returned an error: ' . $e->getMessage();
             exit;
@@ -37,8 +49,13 @@ class Facebook extends Base
             exit;
         }
 
-        return $this->format($data);
+        //get the page from all the pages if there are.
+        $totalData = $this->paginate($data);
+
+        //format the data to be processed by the etl package.
+        return $this->format($totalData);
     }
+
 
     /**
      * Formats data
@@ -50,12 +67,45 @@ class Facebook extends Base
      */
     private function format($data) :array
     {
-        $reviews = $data->asArray()['ratings'] ?? [];
-        foreach ($reviews as &$r) {
-            $r['created_time'] = $r['created_time']->format(DateTimeInterface::ISO8601);
-            $r['reviewer'] = $r['reviewer']['name'];
+        $formattedData = [];
+        foreach ($data as $key => $value) {
+            $formattedData[] = [
+              'rating' => $value['rating'],
+              'has_review' => $value['has_review'],
+              'reviewer' => $value['reviewer']['name'],
+              'recommendation_type' => $value['recommendation_type'],
+              'review_text' => $value['review_text'],
+              'language' => $value['open_graph_story']['language'],
+              'reviewDate' => $value['created_time']->format(\DateTimeInterface::ISO8601)
+          ];
         }
 
-        return $reviews;
+        return $formattedData;
+    }
+
+
+    /**
+     * Paginates facebook data
+     *
+     * @param  Object facebook $data
+     * @return array
+     */
+    public function paginate($data):array
+    {
+        $totalData = [];
+
+        if ($this->library->next($data)) {
+            $reviewsArray = $data->asArray();
+            $totalReviews = array_merge($totalData, $reviewsArray);
+            while ($data = $this->library->next($data)) {
+                $reviewsArray = $data->asArray();
+                $totalReviews = array_merge($totalData, $reviewsArray);
+            }
+        } else {
+            $reviewsArray = $data->asArray();
+            $totalData = array_merge($totalData, $reviewsArray);
+        }
+
+        return $totalData;
     }
 }
