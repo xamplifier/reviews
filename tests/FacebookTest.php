805d1bf7e36170920312f7a618f0cd8b5ae1f82d
Antonis Flangofas <antonisflags@gmail.com>
2019-05-08 15:16:22 +0300

    Removed incomplete test case (xm-1176)

--- tests/FacebookTest.php
+++ tests/FacebookTest.php
@@ -10,16 +10,5 @@ class FacebookTest extends TestCase
 
     public function testCanInstantiateFacebookObj()
     {
-        $e = new EndPoint([
-            'token' => 'EAAHXTZCLCmYsBABQQkYLCCrVXPoav6Y5ZBKERFhRFyEymyxPzW8xt09U9O65exxxcJTbQIOCrQYkLkWd9HEtuaGPnp14V2AllHhvZAvAJDpDSdVLiHeTdtzjcg9XoOSk85fdfbqzbqFZBGrAzzefbB9OT7pDHDJ2q4l0JYYb3q1NjnrXEyH3OdBpUCTAY4AZD',
-            // 'name' => 'Vein Clinics of America - Baltimore',
-            'siteid' => '184680898972315',
-            // 'id' => '59c3a67c1682fa6cd5d507e1',
-        ]);
-
-        $fbApi = new FacebookApi();
-
-        $result = $fbApi->getReviews($e);
-        dd($result);
     }
 }
